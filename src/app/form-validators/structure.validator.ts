b20458f87994523718fdd5f1672c8f7ef5fc1027
Male Michael <malmike21@outlook.com>
2017-08-09 18:51:52 +0300

    Added a directure for struture directive It notes each change in the structure as the value changes and calls structure validator to verify that it  to set regular expressions

--- src/app/form-validators/structure.validator.ts
+++ src/app/form-validators/structure.validator.ts
@@ -1,4 +1,5 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
 
 export function StructureValidator(structureRe: Array<RegExp>): ValidatorFn{
     return (control: AbstractControl): {[key: string]: any} => {
@@ -16,5 +17,37 @@ export function StructureValidator(structureRe: Array<RegExp>): ValidatorFn{
 
             return validate ? null: {'forbiddenStructure': {value}};
         }
+    }
+}
+
+
+@Directive({
+    selector: '[forbiddenStructure]',
+    providers: [{provide: NG_VALIDATORS, useExisting: StructureValidatorDirective, multi: true}]
+})
+export class StructureValidatorDirective implements Validator, OnChanges {
+    @Input() forbiddenStructure: Array<string>;
+    private valFn = Validators.nullValidator;
+
+    ngOnChanges(changes: SimpleChanges): void{
+        let values: Array<RegExp> = new Array<RegExp>();
+        const change: Array<string> = changes['forbiddenStructure'].currentValue;
+        if(change){
+            change.forEach(value => {
+                const val: any= value;
+                const re: RegExp = val instanceof RegExp ? val : new RegExp(val, 'i');
+                values.push(re);
+            })
+
+            if(values.length > 0){
+                this.valFn = StructureValidator(values);
+            }
+        }else{
+                    this.valFn = Validators.nullValidator;
+        }
+    }
+
+    validate(control: AbstractControl): {[key: string]: any} {
+        return this.valFn(control);
     }
 }
