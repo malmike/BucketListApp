16ea9521a2cf51ab4f1d731671e07d4f464a211c
Male Michael <malmike21@outlook.com>
2017-08-09 09:43:13 +0300

    Added route for login in the app-routing module

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-      {path: '', redirectTo: 'registration', pathMatch: 'full'},
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: 'login', redirectTo: 'login', pathMatch: 'full'},
       {path: 'registration', redirectTo: 'registration', pathMatch: 'full'},
 ];
 
