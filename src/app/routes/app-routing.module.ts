5c453e218a540fe3a8c58274d6faab5d6d074a7a
Male Michael <malmike21@outlook.com>
2017-08-09 09:27:09 +0300

    Added call for the registration route in app-routing module

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
+      {path: '', redirectTo: 'registration', pathMatch: 'full'},
+      {path: 'registration', redirectTo: 'registration', pathMatch: 'full'},
 ];
 
 @NgModule({
