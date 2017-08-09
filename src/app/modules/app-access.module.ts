09c0641ed6558c18cc7ef0255168a1829c55ee3e
Male Michael <malmike21@outlook.com>
2017-08-09 09:07:14 +0300

    Declared app access routing module in the app access module

--- src/app/modules/app-access.module.ts
+++ src/app/modules/app-access.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule  } from '@angular/forms';
 
+//Routing Modules
+import { AppAccessRoutingModule } from '../routes/app-access-routing.module';
+
 @NgModule({
     imports: [
         CommonModule,
         HttpModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        AppAccessRoutingModule
     ],
     declarations: [
     ],
