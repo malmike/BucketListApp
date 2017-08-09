97b79b596d489772d12e289023776eed46e069ad
Male Michael <malmike21@outlook.com>
2017-08-09 22:37:36 +0300

    Fixed routing issue that was causing components to stay on page after routing

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-      {path: '', redirectTo: 'login', pathMatch: 'full'},
-      {path: 'login', redirectTo: 'login', pathMatch: 'full'},
-      {path: 'registration', redirectTo: 'registration', pathMatch: 'full'},
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'registration', redirectTo: '/registration', pathMatch: 'full'},
 ];
 
 @NgModule({
