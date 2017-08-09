4435caa75b42f79b14873534b871e73ad6f3fa76
Male Michael <malmike21@outlook.com>
2017-08-09 14:33:41 +0300

    Removed unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 // External modules and dependencies
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import 'hammerjs';
 
 // Routing modules
 import { AppRoutingModule } from './routes/app-routing.module'
@@ -19,9 +16,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
+    BrowserModule,
     AppRoutingModule,
     AppAccessModule
   ],
