ce1b40fe2ebb2822fd9435fd5d625f1341fe55fb
Male Michael <malmike21@outlook.com>
2017-08-09 08:49:36 +0300

    Created initial strucutre of app access module

---
+++ src/app/modules/app-access.module.ts
@@ -0,0 +1,18 @@
+//External modules and dependencies
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule  } from '@angular/forms';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        HttpModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+    ],
+    providers: [
+    ]
+})
+export class AppAccessModule { }
