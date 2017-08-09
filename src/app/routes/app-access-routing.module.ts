258bcaf30995b3b99df4780e5c61fc54c301178d
Male Michael <malmike21@outlook.com>
2017-08-09 09:18:11 +0300

    Added route for registration component in app access routing module

--- src/app/routes/app-access-routing.module.ts
+++ src/app/routes/app-access-routing.module.ts
@@ -1,11 +1,16 @@
+// External modules and dependencies
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// Components
+import { RegistrationComponent } from '../components/app-access/registration/registration.component';
 
 @NgModule({
   imports: [RouterModule.forChild([
+      { path: 'registration', component: RegistrationComponent}
   ])],
   exports: [RouterModule]
 })
+
 export class AppAccessRoutingModule {}
 
