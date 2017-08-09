82fbefeb8331404c637455013024212878838c5f
Male Michael <malmike21@outlook.com>
2017-08-09 09:42:10 +0300

    Added route of login component in app-access routing module

--- src/app/routes/app-access-routing.module.ts
+++ src/app/routes/app-access-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // Components
+import { LoginComponent } from '../components/app-access/login/login.component';
 import { RegistrationComponent } from '../components/app-access/registration/registration.component';
 
 @NgModule({
   imports: [RouterModule.forChild([
-      { path: 'registration', component: RegistrationComponent}
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent }
   ])],
   exports: [RouterModule]
 })
