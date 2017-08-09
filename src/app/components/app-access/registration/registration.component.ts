455b38e524a1126dbd8cbb988a95e36738fbb5d2
Male Michael <malmike21@outlook.com>
2017-08-09 21:14:24 +0300

    Implemented the OnInit interface in the Registration Component This will call the method for building the registration form and form validation

--- src/app/components/app-access/registration/registration.component.ts
+++ src/app/components/app-access/registration/registration.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+// External Dependencies
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdSnackBar } from '@angular/material';
+
+//Form Validators
+import { StructureValidator } from '../../../form-validators/structure.validator';
+
+// Services
+import { RegExpService } from '../../../services/shared-information/reg-exp.service';
+import { WebApiPathService } from '../../../services/shared-information/webapi-path.service';
+import { RegistrationService } from '../../../services/http-calls/registration.service';
+
+// Models
+import { UserModel } from '../../../models/user.model';
 
 @Component({
-  selector: 'registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+    selector: 'registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.css']
 })
 
-export class RegistrationComponent {
-  title = 'BucketListApp Registration';
+export class RegistrationComponent implements OnInit{
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
 }
