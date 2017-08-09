98ddfa7b6aea67e48639a1d073a0ba794c9b52f4
Male Michael <malmike21@outlook.com>
2017-08-09 15:00:34 +0300

    Added user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+export class UserModel{
+    constructor(
+        public fname?: string,
+        public lname?: string,
+        public email?: string,
+        public password?: string
+    ){}
+}
