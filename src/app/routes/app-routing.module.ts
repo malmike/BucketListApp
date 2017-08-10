3e4fb186c1d8722fecfda640b1430bd445ef7375
Male Michael <malmike21@outlook.com>
2017-08-10 11:40:32 +0300

    Remove CanLoadGuard from app-routing modue

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,14 +2,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// Guards
-import { CanLoadGuard } from '../guards/router.guard'
-
 export const routes: Routes = [
       {path: '', redirectTo: '/login', pathMatch: 'full'},
       {path: 'login', redirectTo: '/login', pathMatch: 'full'},
       {path: 'registration', redirectTo: '/registration', pathMatch: 'full'},
-      {path: 'nav', loadChildren: 'app/modules/bucketlist.module#BucketlistModule', canLoad: [CanLoadGuard]}
+      {path: 'nav', loadChildren: 'app/modules/bucketlist.module#BucketlistModule'}
 ];
 
 @NgModule({
