bd89c81d4c68d140bdefc508f9da9b5357551f7a
Male Michael <malmike21@outlook.com>
2017-08-10 15:11:17 +0300

    Created a bucketlist model

---
+++ src/app/models/bucketlist.model.ts
@@ -0,0 +1,8 @@
+export class BucketlistModel{
+    constructor(
+        public date_created?: string,
+        public name?: string,
+        public created_by?: number,
+        public date_modified?: string
+    ){}
+}
