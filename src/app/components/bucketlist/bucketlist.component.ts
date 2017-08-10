0d8ad16a7f90a78270097449882d722850307b9a
Male Michael <malmike21@outlook.com>
2017-08-10 10:46:54 +0300

    Added BucketlistComponent class and directive Component

---
+++ src/app/components/bucketlist/bucketlist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bucketlist',
+  templateUrl: './bucketlist.component.html',
+  styleUrls: ['./bucketlist.component.css']
+})
+
+export class BucketlistComponent{}
