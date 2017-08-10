2d2d18afcaa71f092cc2a576eea40db4d3832d57
Male Michael <malmike21@outlook.com>
2017-08-10 09:15:39 +0300

    Added route to bucketlist module

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,10 +1,15 @@
+// External dependencies
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+// Guards
+import { CanLoadGuard } from '../guards/router.guard'
 
 export const routes: Routes = [
       {path: '', redirectTo: '/login', pathMatch: 'full'},
       {path: 'login', redirectTo: '/login', pathMatch: 'full'},
       {path: 'registration', redirectTo: '/registration', pathMatch: 'full'},
+      {path: 'nav', loadChildren: 'app/modules/bucketlist.module#BucketlistModule', canLoad: [CanLoadGuard]}
 ];
 
 @NgModule({
