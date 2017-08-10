b8546c44bb8024e19e8c8a62244364cd50bd9f85
Male Michael <malmike21@outlook.com>
2017-08-10 11:44:28 +0300

    Added routing to bucketlist

--- src/app/routes/bucketlist-routing.module.ts
+++ src/app/routes/bucketlist-routing.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // Guards
-import { CanActivateGuard } from '../guards/router.guard'
+import { CanActivateGuard } from '../guards/router.guard';
+
+//Components
+import { BucketlistComponent } from '../components/bucketlist/bucketlist.component';
 
 @NgModule({
-  imports: [RouterModule.forChild([
+    imports: [RouterModule.forChild([
+        {path: '', component:BucketlistComponent,
+            children:[
+                { path: '', redirectTo:'/bucketlist', pathMatch: 'full'},
+                { path: 'bucketlist', component:BucketlistComponent }]
+        }
 
-  ])],
-  exports: [RouterModule]
+    ])],
+    exports: [RouterModule]
 })
 
 export class BucketlistRoutingModule {}
