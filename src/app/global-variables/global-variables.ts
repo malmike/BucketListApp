f76e78a84256dd1ba45c05374945b6ba0e2092fc
Male Michael <malmike21@outlook.com>
2017-08-11 14:41:18 +0300

    Added loginUrl to the global variables

--- src/app/global-variables/global-variables.ts
+++ src/app/global-variables/global-variables.ts
@@ -2,6 +2,7 @@ export class GlobalVariables {
     private static instance: GlobalVariables;
     private webApi:string = "http://localhost:5000/";
     private token_header:string = "x-access-token";
+    private loginUrl: string = this.webApi + "api/v1/auth/login";
     private constructor() {}
     static getInstance() {
         if (!GlobalVariables.instance) {
@@ -14,5 +15,8 @@ export class GlobalVariables {
     }
     getTokenHeader():string {
         return this.token_header;
+    }
+    getLoginUrl():string {
+        return this.loginUrl;
     }
 }
