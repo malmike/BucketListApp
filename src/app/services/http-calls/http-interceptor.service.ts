39bdb058ad22e2cc3458a333caae08b4c574d4ba
Male Michael <malmike21@outlook.com>
2017-08-11 14:07:11 +0300

    Implemented saveToken method This will update the token saved in the localStorage

--- src/app/services/http-calls/http-interceptor.service.ts
+++ src/app/services/http-calls/http-interceptor.service.ts
@@ -4,12 +4,26 @@ import { Observable } from 'rxjs/Rx';
 
 import { HttpInterceptor } from './http-interceptor';
 
+import { CurrentUserModel } from '../../models/current-user.model';
+
 @Injectable()
 export class HttpInterceptorService extends HttpInterceptor{
+    currentUser: CurrentUserModel = new CurrentUserModel();
     constructor(
         backend: ConnectionBackend,
         defaultOptions: RequestOptions
     ){
         super(backend, defaultOptions);
+    }
+
+    protected saveToken(token: string): Promise<string> {
+        this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+        this.currentUser.token = token;
+        localStorage.setItem("currentUser", JSON.stringify(this.currentUser));
+        throw token;
+    }
+
+    protected refreshToken(): Observable<Response> {
+        throw new Error("Method not implemented.");
     }
 }
