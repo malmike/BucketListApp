814980c784fda5e1bbe88a4dbabe00482dc03bc1
Male Michael <malmike21@outlook.com>
2017-08-11 15:39:38 +0300

    Added global variable store user This will hold the name of the storage object that is stored in local storage

--- src/app/global-variables/global-variables.ts
+++ src/app/global-variables/global-variables.ts
@@ -2,6 +2,7 @@ export class GlobalVariables {
     private static instance: GlobalVariables;
     private webApi:string = "http://localhost:5000/";
     private token_header:string = "x-access-token";
+    private store_user:string = "current_user";
     private loginUrl: string = this.webApi + "api/v1/auth/login";
     private constructor() {}
     static getInstance() {
@@ -18,5 +19,8 @@ export class GlobalVariables {
     }
     getLoginUrl():string {
         return this.loginUrl;
+    }
+    getStoreUser():string {
+        return this.store_user;
     }
 }
