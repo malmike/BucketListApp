bb3013e60efcf4204af16176c5ff968c18994ab2
Male Michael <malmike21@outlook.com>
2017-08-11 23:28:37 +0300

    Added method to help get bucket lists in bucketlist-page component

--- src/app/components/bucketlist-page/bucketlist-page.component.ts
+++ src/app/components/bucketlist-page/bucketlist-page.component.ts
@@ -6,10 +6,13 @@ import { MdSnackBar } from '@angular/material';
 
 // Models
 import { BucketlistModel } from '../../models/bucketlist.model';
+import { BucketlistPageModel } from '../../models/bucketlist_page.model';
 import { CurrentUserModel } from '../../models/current-user.model';
+import { UserModel } from '../../models/user.model';
 
 // Services
 import { AddBucketlistService } from '../../services/http-calls/add-bucketlist.service';
+import { GetBucketlistService } from '../../services/http-calls/get-bucketlists.service'
 import { WebApiPathService } from '../../services/shared-information/webapi-path.service';
 
 // Global Variables
@@ -25,10 +28,16 @@ export class BucketlistPageComponent implements OnInit{
     addbucketlistForm: FormGroup;
     active:boolean = true;
     bucketlist: BucketlistModel = new BucketlistModel();
-    currentUser: CurrentUserModel = new CurrentUserModel();
+    bucketlists: Array<BucketlistModel> = new Array<BucketlistModel>();
+    bucketlist_page: BucketlistPageModel = new BucketlistPageModel();
+    currentUser: CurrentUserModel = JSON.parse(localStorage.getItem(GlobalVariables.getInstance().getStoreUser()));
+    user:UserModel = this.currentUser.user;
+    user_name = this.user.fname+" "+this.user.lname;
+
 
     ngOnInit(): void {
         this.buildForm();
+        this.getBucketLists();
     }
 
     constructor(
@@ -36,7 +45,8 @@ export class BucketlistPageComponent implements OnInit{
         private router: Router,
         private snackBar: MdSnackBar,
         private addBucketlistService: AddBucketlistService,
-        private webApiPathService: WebApiPathService){}
+        private webApiPathService: WebApiPathService,
+        private getBucketlistService: GetBucketlistService){}
 
     buildForm(): void {
          this.addbucketlistForm = this.fb.group({
@@ -82,7 +92,6 @@ export class BucketlistPageComponent implements OnInit{
 
     onSubmitForm(){
         this.bucketlist = this.addbucketlistForm.value;
-        this.currentUser = JSON.parse(localStorage.getItem(GlobalVariables.getInstance().getStoreUser()));
         this.addBucketlistService.addBucketlist(this.bucketlist, this.webApiPathService.getWebApiPath('bucketlist').path, this.currentUser.token)
             .subscribe(response => {
                 if (response.status === "success") {
@@ -103,6 +112,38 @@ export class BucketlistPageComponent implements OnInit{
                 });
                 console.log(errMsg);
             });
+    }
+
+    getBucketLists(query: string = null){
+        let urlPath: string = this.webApiPathService.getWebApiPath('bucketlist').path;
+        if(query !== null ){
+            urlPath += query;
+        }
+        this.getBucketlistService.getBucketlists(urlPath, this.currentUser.token)
+            .subscribe(response => {
+                if (response) {
+                    this.snackBar.open('Bucket List Retrieved,', '', {
+                        duration: 2000,
+                    });
+                    this.bucketlist_page = this.getBucketListPage();
+                    this.bucketlists = this.bucketlist_page.data;
+                }else{
+                    this.snackBar.open('No bucketlists', '', {
+                        duration: 2000,
+                    });
+                    console.log('No bucketlists');
+                }
+            },
+            errMsg => {
+                this.snackBar.open(errMsg, '', {
+                        duration: 2000,
+                });
+                console.log(errMsg);
+            });
+    }
+
+    getBucketListPage(): BucketlistPageModel{
+        return this.getBucketlistService.getBucketlistPage();
     }
 
 }
