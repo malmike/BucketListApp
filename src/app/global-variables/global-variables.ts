1427aeea1d68945df438235daae32d669a5054f8
Male Michael <malmike21@outlook.com>
2017-08-11 13:30:55 +0300

    Added the token_header to the global variables

--- src/app/global-variables/global-variables.ts
+++ src/app/global-variables/global-variables.ts
@@ -1,6 +1,7 @@
 export class GlobalVariables {
     private static instance: GlobalVariables;
     private webApi:string = "http://localhost:5000/";
+    private token_header:string = "x-access-token";
     private constructor() {}
     static getInstance() {
         if (!GlobalVariables.instance) {
@@ -10,5 +11,8 @@ export class GlobalVariables {
     }
     getWebApi():string {
         return this.webApi;
+    }
+    getTokenHeader():string {
+        return this.token_header;
     }
 }
