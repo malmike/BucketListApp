357303ff68815d56692073d0f80907d162ed94c7
Male Michael <malmike21@outlook.com>
2017-08-11 13:46:33 +0300

    Updated requestWithToken method

--- src/app/services/http-calls/http-interceptor.ts
+++ src/app/services/http-calls/http-interceptor.ts
@@ -1,4 +1,4 @@
-import { Http, Response, RequestOptions, RequestOptionsArgs, Headers, ConnectionBackend } from '@angular/http';
+import { Http, Response, RequestOptions, RequestMethod, RequestOptionsArgs, Headers, ConnectionBackend } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/mergeMap';
@@ -6,9 +6,12 @@ import 'rxjs/add/operator/mergeMap';
 
 export abstract class HttpInterceptor extends Http{
     // This is the name assigned to the token header in the api
-    private token_header:string = "x-access-token";
+    ;
     // Variable will hold the request object when there is need to resend it
-    private original_request:Request;
+    private request_method: RequestMethod = null;
+    private request_body: any = null;
+    private request_options: RequestOptionsArgs = null;
+    private request_url: string = null;
 
     constructor(
         backend: ConnectionBackend,
@@ -17,12 +20,19 @@ export abstract class HttpInterceptor extends Http{
         super(backend, defaultOptions);
     }
 
-    private requestWithToken(req: Request, token: string, header: string): Observable<Response>{
+    private requestWithToken(
+        req_method: RequestMethod, req_body: any, req_options: RequestOptionsArgs,
+        req_url: string, token: string, header: string): Observable<Response>{
         if(!token){
             return Observable.throw(new Error("No token provided"));
         }
-        req.headers.set(header, token);
-        console.log(req.method)
+        req_options.headers.set(header, token);
+
+        if (req_method === RequestMethod.Get) return super.get(req_url, req_options);
+        if(req_method === RequestMethod.Post) return super.post(req_url, req_body, req_options);
+        if(req_method === RequestMethod.Put) return super.put(req_url, req_body, req_options);
+        if(req_method === RequestMethod.Delete) return super.delete(req_url, req_options);
+
         return Observable.throw(new Error("Testing 123"));
     }
 
@@ -38,27 +48,41 @@ export abstract class HttpInterceptor extends Http{
     }
 
     get(url: string, options?: RequestOptionsArgs):Observable<Response>{
+        this.request_url = url;
+        this.request_options = options;
+        this.request_method = RequestMethod.Get;
         console.log('Haha, using my get');
         return this.intercept(super.get(url, options));
     }
 
     post(url: string, body:any, options?: RequestOptionsArgs):Observable<Response>{
+        this.request_url = url;
+        this.request_options = options;
+        this.request_method = RequestMethod.Post;
+        this.request_body = body;
         console.log('Haha, using my post');
-        return this.intercept(super.post(url, options));
+        return this.intercept(super.post(url, body, options));
     }
 
     put(url: string, body:any, options?: RequestOptionsArgs):Observable<Response>{
+        this.request_url = url;
+        this.request_options = options;
+        this.request_method = RequestMethod.Put;
+        this.request_body = body;
         console.log('Haha, using my put');
-        return this.intercept(super.put(url, options));
+        return this.intercept(super.put(url, body, options));
     }
 
     delete(url: string, body:any, options?: RequestOptionsArgs):Observable<Response>{
+        this.request_url = url;
+        this.request_options = options;
+        this.request_method = RequestMethod.Delete;
+        this.request_body = body;
         console.log('Haha, using my delete');
         return this.intercept(super.delete(url, options));
     }
 
     private intercept(observable: Observable<Response>): Observable<Response>{
-        console.log('Intercept works');
         return observable;
     }
 
