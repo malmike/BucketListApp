796fd59445796e3b6370df43f33760102c534bd5
Male Michael <malmike21@outlook.com>
2017-08-12 00:53:58 +0300

    Added BuckelistItemComponent class

---
+++ src/app/components/bucketlist-item/bucketlist-item.component.ts
@@ -0,0 +1,16 @@
+// External Dependencies
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MdSnackBar } from '@angular/material';
+
+
+@Component({
+    selector: 'bucketlistitem',
+    templateUrl: './bucketlist-item.component.html',
+    styleUrls: ['./bucketlist-item.component.css']
+})
+
+export class BucketlistItemComponent implements OnInit{
+    ngOnInit(): void {
+    }
+}
