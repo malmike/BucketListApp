514e85d668d9b3aa2a36bd93662bec55f0522804
Male Michael <malmike21@outlook.com>
2017-08-12 15:34:58 +0300

    Retreive the bucketlist stored in shared bucketlist service

--- src/app/components/bucketlist-item/bucketlist-item.component.ts
+++ src/app/components/bucketlist-item/bucketlist-item.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MdSnackBar } from '@angular/material';
 
+// Services
+import { SharedBucketlistService } from '../../services/shared-information/shared-bucketlist.service';
+
+// Models
+import { BucketlistModel } from '../../models/bucketlist.model';
 
 @Component({
     selector: 'bucketlistitem',
@@ -11,6 +16,23 @@ import { MdSnackBar } from '@angular/material';
 })
 
 export class BucketlistItemComponent implements OnInit{
+
+    private bucketlist: BucketlistModel = new BucketlistModel();
+    private edit: boolean = false;
+
+    constructor(
+        private sharedBucketlistService: SharedBucketlistService
+    ){}
+
     ngOnInit(): void {
+        this.getBucketlist();
+    }
+
+    getBucketlist(){
+        this.bucketlist = this.sharedBucketlistService.getBucketlist();
+    }
+
+    canEdit(){
+        this.edit = true;
     }
 }
