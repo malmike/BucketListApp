55314a605a63e6c2f8ac7899d0ea12c001e37b53
Male Michael <malmike21@outlook.com>
2017-08-20 20:41:45 +0300

    Added an array of bucketlist items in bucketlist model

--- src/app/models/bucketlist.model.ts
+++ src/app/models/bucketlist.model.ts
@@ -1,7 +1,9 @@
+import { BucketlistItemModel } from './bucketlist_item.model';
 export class BucketlistModel{
     constructor(
         public id?: number,
         public date_created?: string,
+        public bucketlist_items?: Array<BucketlistItemModel>,
         public name?: string,
         public created_by?: number,
         public date_modified?: string
