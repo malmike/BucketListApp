6fc074e9efca5f2693e7a5a5409f880d42fe46aa
Male Michael <michael.male@andela.com>
2017-09-19 11:50:13 +0300

    feat(add-item-dialog) modify buckelist module - add add-item-dialog component and add-item-dialog serivice to bucketlist module

--- src/app/modules/bucketlist.module.ts
+++ src/app/modules/bucketlist.module.ts
@@ -1,9 +1,10 @@
 // External Modules and Dependencies
-import { NgModule } from '@angular/core';
+import { NgModule, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Http, RequestOptions, ConnectionBackend, XHRBackend } from '@angular/http';
+import { MyDatePickerModule } from 'mydatepicker';
 import 'hammerjs';
 
 // Modules
@@ -13,6 +14,7 @@ import { CustomMaterialModule } from './custom-material.module';
 import { BucketlistComponent } from '../components/bucketlist/bucketlist.component';
 import { BucketlistPageComponent } from '../components/bucketlist-page/bucketlist-page.component';
 import { BucketlistItemComponent } from '../components/bucketlist-item/bucketlist-item.component';
+import { AddItemDialogComponent } from '../components/add-item-dialog-component/add-item-dialog.component';
 
 // Routing Modules
 import { BucketlistRoutingModule } from '../routes/bucketlist-routing.module';
@@ -22,6 +24,7 @@ import { Permissions, CanActivateGuard } from '../guards/router.guard';
 
 // Service
 import { HttpInterceptorService } from '../services/http-calls/http-interceptor.service';
+import { AddBucketlistItemService} from '../services/http-calls/add-bucketlist-item.service';
 import { AddBucketlistService } from '../services/http-calls/add-bucketlist.service';
 import { GetBucketlistsService } from '../services/http-calls/get-bucketlists.service';
 import { GetBucketlistService } from '../services/http-calls/get-bucketlist.service';
@@ -29,6 +32,9 @@ import { DeleteBucketlistService } from '../services/http-calls/delete-bucketlis
 import { WebApiPathService } from '../services/shared-information/webapi-path.service';
 import { GetUserDetails } from '../services/shared-information/user-details.service';
 import { UpdateBucketlistService } from '../services/http-calls/update-bucketlist.service';
+import { AddItemDialogService } from '../services/dialogs/add-item-dialog.service';
+import { MdDialog } from '@angular/material';
+
 
 //Models
 import { CurrentUserModel } from '../models/current-user.model';
@@ -38,6 +44,10 @@ import { GlobalVariables } from '../global-variables/global-variables';
 
 export function getHttpInterceptor(backend: ConnectionBackend, defaultOptions: RequestOptions){
     return new HttpInterceptorService(backend, defaultOptions);
+}
+
+export function addItemDialgService(dialog: MdDialog){
+    return new AddItemDialogService(dialog);
 }
 
 export function getCurrentUser(){
@@ -52,9 +62,14 @@ export function getCurrentUser(){
         FormsModule,
         ReactiveFormsModule,
         BucketlistRoutingModule,
-        CustomMaterialModule
+        CustomMaterialModule,
+        MyDatePickerModule
+    ],
+    exports:[
+        AddItemDialogComponent
     ],
     declarations: [
+        AddItemDialogComponent,
         BucketlistComponent,
         BucketlistPageComponent,
         BucketlistItemComponent
@@ -66,17 +81,26 @@ export function getCurrentUser(){
             deps:[XHRBackend, RequestOptions]
         },
         {
+            provide: AddItemDialogService,
+            useFactory: addItemDialgService,
+            deps:[MdDialog]
+        },
+        {
             provide: GetUserDetails,
             useFactory: getCurrentUser
         },
         Permissions,
         CanActivateGuard,
         AddBucketlistService,
+        AddBucketlistItemService,
         GetBucketlistsService,
         GetBucketlistService,
         UpdateBucketlistService,
         DeleteBucketlistService,
         WebApiPathService
+    ],
+    entryComponents: [
+        AddItemDialogComponent
     ]
 })
 
