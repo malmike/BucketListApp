039f9fb7f98107c5e8747b85d4c7a996b06ba2fb
Male Michael <michael.male@andela.com>
2017-09-19 11:47:21 +0300

    feat(add-item-dialog) modify custom material module - import extra material modules that are to be used in the application

--- src/app/modules/custom-material.module.ts
+++ src/app/modules/custom-material.module.ts
@@ -1,10 +1,10 @@
 //External modules and dependencies
 import { NgModule } from '@angular/core';
-import { MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule } from '@angular/material';
+import { MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule } from '@angular/material';
 
 @NgModule({
-  imports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule],
-  exports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule]
+  imports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule],
+  exports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule]
 })
 
 export class CustomMaterialModule { }
