7a442719b82e30dd37ec5b62e49ed26a8a52a3e8
Male Michael <michael.male@andela.com>
2017-09-20 12:21:07 +0300

    feat(update-item) add update item dialog component - add compnent that structures the dialog for update item

--- src/app/modules/custom-material.module.ts
+++ src/app/modules/custom-material.module.ts
@@ -1,10 +1,10 @@
 //External modules and dependencies
 import { NgModule } from '@angular/core';
-import { MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule } from '@angular/material';
+import { MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule, MdCheckboxModule } from '@angular/material';
 
 @NgModule({
-  imports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule],
-  exports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule]
+  imports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule, MdCheckboxModule],
+  exports: [MdCardModule, MdInputModule, MdButtonModule, MdSnackBarModule, MdDialogModule, MdIconModule, MdCheckboxModule]
 })
 
 export class CustomMaterialModule { }
