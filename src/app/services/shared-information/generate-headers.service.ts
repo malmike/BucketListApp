6c03fc58065ae226ad6c9cc09ff93dab04fe41c1
Male Michael <michael.male@andela.com>
2017-09-21 06:26:55 +0300

    chore(refactor) add service to generate headers

---
+++ src/app/services/shared-information/generate-headers.service.ts
@@ -0,0 +1,23 @@
+import { Injectable }    from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { GlobalVariables } from '../../global-variables/global-variables';
+import { GetUserDetails } from './user-details.service';
+
+@Injectable()
+export class GenerateHeadersService {
+
+    constructor(
+        private user_details: GetUserDetails){}
+
+    public getHeaders(get_token?:boolean) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        if(get_token)headers.append(GlobalVariables.getInstance().getTokenHeader(), this.user_details.gettoken());
+        headers.append('Accept', 'application/json');
+        let requestoptions = new RequestOptions({
+            headers: headers
+        });
+        return requestoptions;;
+    }
+}
