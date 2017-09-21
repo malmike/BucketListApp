752e09c7778c6990fecb01c7ee7795a650d18502
Male Michael <michael.male@andela.com>
2017-09-21 06:27:38 +0300

    chore(refactor) add service to handle errors - handles errors after an http call

---
+++ src/app/services/shared-information/handle-errors.service.ts
@@ -0,0 +1,23 @@
+import { Injectable }    from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HandleErrorsService {
+    public handleError (error: Response | any, change_message?: boolean) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || 'Failed';
+            const err = body.message || JSON.stringify(body);
+            errMsg = `${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        if(change_message && error.status === 404){
+            errMsg = "User has no single bucketlist";
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+}
