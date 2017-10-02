b2e402136ef7f142a30702dd7f84e4bc3a7676a2
Male Michael <michael.male@andela.com>
2017-10-02 11:40:57 +0300

    Added circleci configurations

--- src/app/components/add-item-dialog-component/add-item-dialog.component.spec.ts
+++ src/app/components/add-item-dialog-component/add-item-dialog.component.spec.ts
@@ -9,14 +9,14 @@ import { BucketlistRoutingModule } from '../../routes/bucketlist-routing.module'
 import { AppRoutingModule } from '../../routes/app-routing.module';
 import { MyDatePickerModule } from 'mydatepicker';
 
-//components
+// components
 import { AddItemDialogComponent } from './add-item-dialog.component';
 import { BucketlistComponent } from '../bucketlist/bucketlist.component';
 import { BucketlistPageComponent } from '../bucketlist-page/bucketlist-page.component';
 import { BucketlistItemComponent } from '../bucketlist-item/bucketlist-item.component';
 import { AppComponent } from '../../app.component';
 
-//services
+// services
 import { WebApiPathService } from '../../services/shared-information/webapi-path.service';
 import { GetUserDetails } from '../../services/shared-information/user-details.service';
 import { GenerateHeadersService } from '../../services/shared-information/generate-headers.service';
@@ -25,10 +25,10 @@ import { AddItemDialogService } from '../../services/dialogs/add-item-dialog.ser
 import { AddBucketlistItemService } from '../../services/http-calls/add-bucketlist-item.service';
 import { MockAddBucketlistItemService } from '../../tests/mock-add-bucketlist-item-service.spec';
 
-//global variables
+// global variables
 import { GlobalVariables } from '../../global-variables/global-variables';
 
-//test data
+// test data
 import { TestToken, TestUser} from '../../tests/test.data.spec';
 import { MockGetBucketlistService } from '../../tests/mock-get-bucketlist-service.spec';
 
@@ -58,7 +58,7 @@ describe('Add Item Dialog', () => {
                 BucketlistPageComponent,
                 BucketlistItemComponent
             ],
-            providers:[
+            providers: [
                 {provide: APP_BASE_HREF, useValue: '/my/app'},
                 WebApiPathService,
                 GenerateHeadersService,
@@ -78,7 +78,7 @@ describe('Add Item Dialog', () => {
         fixture = TestBed.createComponent(AddItemDialogComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    })
+    });
 
     it('should be created', () => {
         expect(component).toBeTruthy();
--- src/app/tests/mock-get-bucketlists-service.spec.ts
+++ src/app/tests/mock-get-bucketlists-service.spec.ts
@@ -1,19 +1,19 @@
-import{ Observable } from "rxjs";
+import { Observable } from 'rxjs/Rx';
 
-import { TestBucketlistPage } from "./test.data.spec";
+import { TestBucketlistPage } from './test.data.spec';
 import { BucketlistPageModel } from '../models/bucketlist_page.model';
 import { ResponseModel } from '../models/response.model';
 
 export class MockGetBucketlistsService {
     bucketlist_page: BucketlistPageModel = new BucketlistPageModel();
     response: ResponseModel = {
-        message: "Bucketlists retrieved",
-        status: "success"
-    }
+        message: 'Bucketlists retrieved',
+        status: 'success'
+    };
 
-    public getBucketlists(path: string, token: string): Observable<ResponseModel>{
+    public getBucketlists(path: string, token: string): Observable<ResponseModel> {
         this.bucketlist_page = TestBucketlistPage;
-        return Observable.of(this.response)
+        return Observable.of(this.response);
     }
 
     public getBucketlistPage(): BucketlistPageModel {
